a430677dcbfbf6136d6fdd090d14524d7fc92cd1
Kamiλ Shakirov <ska80@users.noreply.github.com>
2022-04-27 18:20:48 +0600

    Update config.scm

--- config.scm
+++ config.scm
@@ -1,56 +1,74 @@
-;; This is an operating system configuration generated
-;; by the graphical installer.
-
-(use-modules (gnu)
+(use-modules (gnu packages)
              (nongnu packages linux)
              (nongnu system linux-initrd))
+
 (use-service-modules desktop networking ssh xorg)
 
 (operating-system
   (kernel linux)
   (initrd microcode-initrd)
   (firmware (list linux-firmware))
+ 
+  (host-name "guixbox")
   (locale "en_US.utf8")
   (timezone "Asia/Bishkek")
-  (keyboard-layout (keyboard-layout "us"))
-  (host-name "guixbox")
+  (keyboard-layout (keyboard-layout "us" #:model "thinkpad"))
+ 
   (users (cons* (user-account
                   (name "kamil")
                   (comment "Kamil Shakirov")
                   (group "users")
                   (home-directory "/home/kamil")
                   (supplementary-groups
-                    '("wheel" "netdev" "audio" "video")))
+                    '("wheel" "netdev" "audio" "video"))
+                  (shell (file-append zsh "/bin/zsh")))
                 %base-user-accounts))
+
   (packages
     (append
-      (list (specification->package "emacs")
-            (specification->package "emacs-exwm")
-            (specification->package
-              "emacs-desktop-environment")
-            (specification->package "nss-certs"))
+      (map specification->package
+        '("zsh" "mc"
+          "htop" "powertop"
+          "emacs" "vim"
+          "ffmpeg" "mpv"
+          "gnupg@2.0" "nss-certs"))
       %base-packages))
+  
   (services
     (append
-      (list (service openssh-service-type)
+      (list (service openssh-service-type
+              (openssh-configuration
+                (port-number 711)
+                (permit-root-login #f)
+                (allow-empty-passwords? #f)))
             (service tor-service-type)
             (set-xorg-configuration
               (xorg-configuration
                 (keyboard-layout keyboard-layout))))
       %desktop-services))
+ 
   (bootloader
     (bootloader-configuration
       (bootloader grub-efi-bootloader)
       (target "/boot/efi")
       (keyboard-layout keyboard-layout)))
+ 
+  (swap-devices
+    (list (uuid "510e1e76-8337-4db0-ad6d-cc90228de721")))
+ 
   (mapped-devices
     (list (mapped-device
             (source
               (uuid "17eac23d-3f36-4ea4-aa99-537571620d0a"))
             (target "system-root")
             (type luks-device-mapping))))
+ 
   (file-systems
     (cons* (file-system
+             (mount-point "/boot/efi")
+             (device (uuid "F872-B72A" 'fat)            
+             (type "vfat")))
+           (file-system
              (mount-point "/")
              (device "/dev/mapper/system-root")
              (type "ext4")
